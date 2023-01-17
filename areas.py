3993182bcd948b2d356f50f865f99b0e61569ca6
RCANDRESS <aramirez231@estudiantes.areandina.edu.co>
2023-01-17 18:14:05 -0500

    Tercera modificacion

--- areas.py
+++ areas.py
@@ -5,3 +5,9 @@ def areaCuadrado():
     lado=float(input("ingrese el valor del lado: "))
     return print("El area del cuadrado es: ", lado**2)
 areaCuadrado()
+
+def areaRectangulo():
+    base=float(input("ingrese el valor de la base: "))
+    altura=float(input("ingrese el valor de la altura: "))
+    return print("El area del rectangulo es: ", base * altura)
+areaRectangulo()
