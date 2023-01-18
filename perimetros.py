25c7233a051a45f9534c314e40a06ded40e6ca44
RCANDRESS <aramirez231@estudiantes.areandina.edu.co>
2023-01-17 21:40:19 -0500

    segundo rama nueva

---
+++ perimetros.py
@@ -0,0 +1,19 @@
+def perCuadrado():
+    lado=float(input("ingrese el valor del perimetro: "))
+    return print("El area del cuadrado es: ", lado*4)
+perCuadrado()
+
+
+def perRectangulo():
+    base=float(input("ingrese el valor de la base: "))
+    altura=float(input("ingrese el valor de la altura: "))
+    return print("El area del rectangulo es: ", 2 * base + 2* altura)
+perRectangulo()
+
+pi=3,1416
+
+def perCirculo():
+    radio=float(input("ingrese el valor del radio: "))
+    altura=float(input("ingrese el valor de la altura: "))
+    return print("El area del circulo es: ", 2+pi * radio)
+perCirculo()
